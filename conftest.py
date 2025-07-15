be308620de6e4c455b8df510796dd507ef04266a
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-07-15 16:04:15 +0300

    Prepare the testing environment and add initial project files

---
+++ conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+def pytest_addoption(parser):
+    """Добавление опций командной строки для выбора браузера и языка."""
+
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru, en, es, fr, etc.")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    """Фикстура для запуска браузера с заданными параметрами."""
+
+    # Получаем параметры командной строки
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    print(f"\nstart {browser_name} browser for test..")
+    # Устанавливаем настройки браузера для языка
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    # Инициализируем браузер в зависимости от выбранного
+    if browser_name == "chrome":
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        # Если браузер не поддерживается, выбрасываем исключение
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    print("\nstart browser for test..")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
