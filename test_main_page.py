170cbe970f81f8092ce7ffb84fb57ce15c695e8c
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-07-17 13:07:14 +0300

    Added MainPage(BasePage) for the main site page; updated test_main_page.py to use Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,8 @@ import pytest
 import selenium
 from selenium.webdriver.common.by import By
 
+from .pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 def go_to_login_page(browser):
 
@@ -9,5 +11,8 @@ def go_to_login_page(browser):
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser=browser, url=link) # инициализируем Page Object, п
+    # ередаем в конструктор экземпляр драйвера и url адрес
+
+    page.open()  # открываем страницу
+    page.go_to_login_page()   # выполняем метод страницы — переходим на страницу логина
