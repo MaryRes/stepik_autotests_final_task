25e6156f8da1446d6083a17e459c6cede53905d3
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-07-17 16:08:07 +0300

    [Test] Add LoginPage and MainPage tests and error messages

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,8 @@
+from selenium.common import InvalidSelectorException, NoSuchElementException
+
 from .base_page import BasePage
+from .locators import LoginPageLocators
+import selenium
 
 
 class LoginPage(BasePage):
@@ -9,12 +13,25 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        current_url = self.browser.current_url
+        login = "login"
+        assert login in current_url.lower(), "absence of login word in url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        # проверка, что есть форма логина
+        try:
+            login_form = BasePage.is_element_present(self, *LoginPageLocators.LOGIN_FORM)
+            message = "Login form is not present"  # <- message должен быть всегда
+        except InvalidSelectorException as e:
+            login_form = False
+            message = f"Invalid selector: {LoginPageLocators.LOGIN_FORM}"
+        except NoSuchElementException as e:
+            login_form = False
+            message = "Login form not found"
+
+        assert login_form, message
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        register_form = BasePage.is_element_present(self, *LoginPageLocators.REGISTRATION_FORM)
+        assert register_form, "absence of registration form"
