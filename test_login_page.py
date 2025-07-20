f333aa3885fe21c0906776aac61d2606dff32db9
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-07-20 08:51:46 +0300

    [Test] Removed test_should_be_login_page and added test_guest_should_go_to_login_page

--- test_login_page.py
+++ test_login_page.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 
 from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 from .pages.locators import MainPaigeLocators, LoginPageLocators
 
@@ -11,9 +12,19 @@ registration_form_element = LoginPageLocators.REGISTRATION_FORM
 
 # pytest -v --tb=line test_login_page.py
 
-link = "http://selenium1py.pythonanywhere.com/fi/accounts/login/"
+login_page_link = "http://selenium1py.pythonanywhere.com/fi/accounts/login/"
+link = "http://selenium1py.pythonanywhere.com"
+
 def test_should_be_login_page(browser):
     # НЕ ПОНЯТНО
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
+
+
+def test_guest_should_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
