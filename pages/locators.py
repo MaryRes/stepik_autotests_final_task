a5c18b38928c69d69c28a03a62d353aee9682d38
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-08-16 22:57:16 +0300

    [Chore]Add locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -9,7 +9,29 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, '[class*="btn-add-to-basket"]')
-    PRODUCT_NAME = (By.CSS_SELECTOR, '[class*=product_main] h1')
-    PRODUCT_PRICE = (By.CSS_SELECTOR, '[class*=price_color]')
+    # до и после клика на add_to_basket
+    PRODUCT_NAME = (By.CSS_SELECTOR, '[class*=product_main] h1')  # до и после клика один и тот же селектор
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '[class*=price_color]')  # до и после клика один и тот же селектор
+    # сообщения, 1 из них содержит название книги и текст, что она добавлена в корзину
     BASKET_MESSAGE_BOX = (By.CSS_SELECTOR, '[id="messages"]')
+    # список элементов "хлебные крошки", в которых последним элементом должно быть название книги
+    BREADCRUMB_BOX = (By.XPATH, '//ul[@class="breadcrumb"]//li[last()]')
+
+    # несколько сообщений: название книги, сообщение о получении бонуса, общая стоимость всей корзины
+    MESSAGE_ELEMENT_STRONG = (By.CSS_SELECTOR, '[class*="alertinner"] strong')
     MESSAGE_ELEMENT = (By.CSS_SELECTOR, '[class*="alertinner"]')
+    PRODUCT_PRICE_INCLUDED_TAX_ROW = (By.XPATH, "//th[text()='Price (incl. tax)']/..//td")
+
+    # Общая цена корзины
+    BASKET_TOTAL_IN_NAVBAR = (By.XPATH, "//a[contains(@href, '/basket') and contains(., 'Total:')]")
+    BASKET_TOTAL_IN_MESSAGE_BOX = (By.XPATH, "//div[@id='messages']//p[contains(., 'basket total')]//strong")
+    # список элементов, которые содержат название продукта (названия должны быть одинаковыми)
+    # элементы могут быть списками элементов, например, MESSAGE_ELEMENT_STRONG
+    list_of_product_titles = [PRODUCT_NAME, MESSAGE_ELEMENT_STRONG, BREADCRUMB_BOX]
+
+    # список элементов, которые содержат цену с НДС за 1 шт. товара
+    list_of_item_prices = [PRODUCT_PRICE, PRODUCT_PRICE_INCLUDED_TAX_ROW]
+
+    # список элементов с общей ценой корзины (может содержать одиночные элементы и списки элементов,
+    # содержащих цену, например, MESSAGE_ELEMENT_STRONG)
+    list_of_basket_total = [BASKET_TOTAL_IN_NAVBAR, BASKET_TOTAL_IN_MESSAGE_BOX]
