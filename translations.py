3ea78239e306762f1a1977b351631845c18fdd65
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-08-16 23:15:12 +0300

    [Chore] Add finnish language

--- translations.py
+++ translations.py
@@ -1,12 +1,23 @@
 translations = {
     'en': {
-        'added_to_basket': "has been added to your basket",
-        'basket_total': "basket total is"
+        'added_to_basket': "has been added to your basket.",
+        'basket_total': "Your basket total is now ",
+        'basket_qualifies_part': "Your basket now qualifies for the  offer."
     },
     'fr': {
-        'added_to_basket': "a été ajouté à votre panier"
+        'added_to_basket': " a été ajouté à votre panier."
     },
     'de': {
-        'added_to_basket': "wurde zu Ihrem Warenkorb hinzugefügt"
-    }
+        'added_to_basket': " wurde zu Ihrem Warenkorb hinzugefügt."
+    },
+    'fi': {
+        'added_to_basket': "on lisätty ostoskoriisi.",
+        'basket_total': "Korisi hinta on nyt yhteensä ",
+        'basket_qualifies_part': "Korisi hyväksyy nyt  -tarjouksen."
+    },
+    'ru': {
+        'added_to_basket': "был добавлен в вашу корзину.",
+        'basket_total': "Стоимость корзины теперь составляет",
+        'basket_qualifies_part': "Ваша корзина удовлетворяет условиям предложения"
+    },
 }
