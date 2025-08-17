dc0d375d51b7cde16aa1489e7119368ded3753d2
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-08-18 00:09:52 +0300

    [Chore] Add BasePageLocators, delete MainPaigeLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,14 @@
 from selenium.webdriver.common.by import By
 
-class MainPaigeLocators:
+
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPaigeLocators:
+    #LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    pass
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, 'form[id*="login"]')
