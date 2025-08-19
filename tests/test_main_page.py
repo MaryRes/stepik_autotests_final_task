339f1cd8c74e7132beebcbdf2f38fb3d26e92457
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-08-19 23:15:21 +0300

    [test][ui] Add empty basket verification test requiring headed mode

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -4,8 +4,10 @@ from selenium.webdriver.common.by import By
 
 from stepik_autotests_final_task.pages.main_page import MainPage
 from stepik_autotests_final_task.pages.login_page import LoginPage
+from stepik_autotests_final_task.pages.basket_page import BasketPage
 
 from stepik_autotests_final_task.pages.locators import MainPaigeLocators, LoginPageLocators
+from stepik_autotests_final_task.conftest import translation_fixture
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -21,6 +23,26 @@ def test_guest_can_go_to_login_page(browser):
 
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+@pytest.mark.ui
+@pytest.mark.headed
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, translation_fixture):
+
+    # Гость открывает главную страницу
+    page = MainPage(browser, link)  # инициализируем Page Object, п
+    page.open()
+
+    # Проверяем, что есть ссылка на корзину в шапке сайта
+    page.should_be_basket_link_in_header()  # выполняем метод страницы — проверяем наличие ссылки на корзину в шапке сайта
+    # Переходит в корзину по кнопке в шапке сайта
+    page.go_to_basket_from_header()  # выполняем метод страницы — переходим в корзину по ссылке в шапке сайта
+    # Проверяем, что мы на странице корзины
+    page.should_be_in_basket_page()  # выполняем метод страницы — проверяем, что мы на странице корзины
+    basket_page = BasketPage(browser, browser.current_url)  # инициализируем Page Object для страницы корзины
+    # сообщение о том, что корзина пуста
+    basket_empty_message = translation_fixture["basket_is_empty"]
+    # Ожидаем, что в корзине нет товаров и есть сообщение о том, что корзина пуста
+    assert basket_page.is_basket_empty(basket_empty_message), "Basket is not empty, but should be empty"
 
 
 def test_guest_should_see_login_link(browser):
