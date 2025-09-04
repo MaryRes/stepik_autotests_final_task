34ec8043fda226e08f278efba6b46cc096a7d933
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-09-04 21:56:35 +0300

    feat(navigation): implement basket page access from header
    Add method to find and click basket link in website header
    Uses BASKET_LINK_IN_HEADER locator for header basket button

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,16 @@
 from selenium.webdriver.common.by import By
 
 from .base_page import BasePage
-from .locators import BasePageLocators
+from .locators import MainPaigeLocators
 
 
 class MainPage(BasePage):
 
-    pass
+    def go_to_basket_page(self):
+        """
+        Находит в шапке сайта ссылну на страницу корзины и кликает на нее
+        :return: None
+        """
+        link = self.browser.find_element(*MainPaigeLocators.BASKET_LINK_IN_HEADER)
+        link.click()
+
