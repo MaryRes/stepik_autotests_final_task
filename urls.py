41101669c8a896989b547f27252a2adf197f3b56
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-09-04 18:14:37 +0300

    refactor(urls): upate PRODUCT_PAGE_URL

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 BASE_URL = "http://selenium1py.pythonanywhere.com"
 LOGIN_PAGE_URL = "http://selenium1py.pythonanywhere.com/fi/accounts/login/"
 MAIN_PAGE_URL = "http://selenium1py.pythonanywhere.com"
-PRODUCT_PAGE_URL = "http://selenium1py.pythonanywhere.com/fi/catalogue/hacking-exposed-wireless_208/"
+PRODUCT_PAGE_URL = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 TEST_PAGE_URL = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 TEST_PROMO_PRODUCT_PAGE_URL = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
