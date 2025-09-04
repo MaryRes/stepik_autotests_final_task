2dbac8f23c4578ee3d65a51b6c85919482126d51
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-09-04 19:34:40 +0300

    refactor(architecture): move login methods to base page for reuse
    Relocate should_be_login_link and go_to_login_page methods from MainPage to BasePage to enable login functionality across all page objects instead of just main page.

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,9 +6,4 @@ from .locators import BasePageLocators
 
 class MainPage(BasePage):
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+    pass
