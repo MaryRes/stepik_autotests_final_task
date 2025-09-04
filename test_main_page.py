135047d052ef05e57ded721266fac3c8c4e692b1
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-09-04 21:47:34 +0300

    test(basket): add empty basket test from main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 import pytest
-from .urls import BASE_URL
+from .urls import BASE_URL, MAIN_PAGE_URL
 from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
 
 
 @pytest.mark.parametrize('url', [BASE_URL])
@@ -15,3 +16,20 @@ def test_should_be_login_link(browser, url):
     page = MainPage(browser, url)
     page.open()
     page.should_be_login_link()
+
+@pytest.mark.new
+@pytest.mark.parametrize('url', [MAIN_PAGE_URL])
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, url):
+
+    # Гость открывает главную страницу
+    page = MainPage(browser, url)
+    page.open()
+    # Переходит в корзину по кнопке в шапке сайта
+    page.go_to_basket_page()
+
+    # Ожидаем, что в корзине нет товаров
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_empty()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_display_empty_basket_message()
+
