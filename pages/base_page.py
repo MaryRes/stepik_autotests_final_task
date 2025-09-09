6e83c262f1b0f9c08f77e3ef6b81997d92f71e81
MaryRes <151729862+MaryRes@users.noreply.github.com>
2025-09-09 14:30:22 +0300

    feat(base_page): add user authorization check method to base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -122,6 +122,10 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
     def should_be_login_link(self):
         """
         Verifies that at least one login link is present with explicit waiting.
